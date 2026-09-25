29a6da2cb2bf41a3be604a81835e77943bff79b9
Artembateishikov <artembatey1337a@gmail.com>
2026-09-25 14:35:20 +0400

    Update var1.py

--- variant1.py
+++ variant1.py
@@ -1,5 +1,4 @@
 from math import *
 x = 2.5
-y = e**cos(x/5) * (sin 3*x +(1+x**3/(0.5*ln x))**0.5
-                   print(y)
-                   
+y = e**cos(x/5) * (sin (3*x) + (1+x**3/(0.5 * log10 (x)))**0.5)
+print(y)
